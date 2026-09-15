a7f84e0371951556a216f2c98ec68f790bfde083
Daniel Tashjian <thewopple@gmail.com>
2026-09-14 20:23:07 -0400

    fixed a bug preventing dry runs with no big query metadata

--- src/liti/core/client/bigquery.py
+++ src/liti/core/client/bigquery.py
@@ -61,11 +61,14 @@ class BqClient:
         self.client.delete_dataset(dataset_ref)
 
     def get_table_item(self, table_ref: bq.TableReference) -> bq.TableListItem | None:
-        for table_item in self.client.list_tables(f'{table_ref.project}.{table_ref.dataset_id}'):
-            if table_item.table_id == table_ref.table_id:
-                return table_item
+        try:
+            for table_item in self.client.list_tables(f'{table_ref.project}.{table_ref.dataset_id}'):
+                if table_item.table_id == table_ref.table_id:
+                    return table_item
 
-        return None
+            return None
+        except NotFound:
+            return None
 
     def has_table(self, table_ref: bq.TableReference) -> bool:
         item = self.get_table_item(table_ref)
